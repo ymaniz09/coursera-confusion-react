7268e4a67204a482fd82c2471f8b0145f4661f61
Ygor <ygor.maniz@gmail.com>
2020-09-03 17:52:02 -0300

    Revert "feat: combining reducers"
    This reverts commit e01e764e80fee4a99c80d8af906ae19f35c34c27.

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,16 +1,9 @@
-import { combineReducers, createStore } from 'redux';
-import { Dishes } from './dishes';
-import { Comments } from './comments';
-import { Promotions } from './promotions';
-import { Leaders } from './leaders';
+import { createStore } from 'redux';
+import { initialState, Reducer } from './reducer';
 
 export const ConfigureStore = () => {
   return createStore(
-    combineReducers({
-      dishes: Dishes,
-      comments: Comments,
-      promotions: Promotions,
-      leaders: Leaders
-    })
+    Reducer,
+    initialState,
   );
 }
